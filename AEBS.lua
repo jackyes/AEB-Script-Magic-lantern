e23bf69af71fc1316e084b7afb728c50c5d93ffc
jackyes <jackyes@users.noreply.github.com>
2017-03-20 15:08:04 +0100

    Update AEBS.lua
    Merged with old code
    Make more shot if Ev Bracket is higher than 3 (5 shoots)

--- AEBS.lua
+++ AEBS.lua
@@ -13,7 +13,8 @@ AEBMenu = menu.new
 
         {
             name = "Ev Bracket",
-            help = "Ev bracket 0 - 5 range. Default: 2",
+            help = "If Ev Bracket is lower or equal 3, take 3 photo.",
+			help2 = "If Ev Bracket is higher than 3, make 5 photo."
             min = 0,
             max = 5,
             value = 2
@@ -31,10 +32,10 @@ AEBMenu.submenu["Run"].select = function(this)
     console.show()
 	print("[---]Hi, AEB is Starting...[---]")
 	msleep(1000)
-	console.hide()
-	--Somthing will happen here! ;)
-	-- EC for the bracket. AEBMenu.submenu["Ev Bracket"].value
+	FocusStack()
 	TakeBlackFrame() --Last thing to do
+	print("[---]Bye. Done[---]")
+	console.hide()
 end
 
 
@@ -60,3 +61,62 @@ function TakeBlackFrame()  --Put a black frame at the and of bracketed image
 	end
 end
 
+function FocusStack()
+    local target = lens.dof_far
+
+    takeshoot()  --First photo at user select (first) focus plane
+
+    while true do
+        if lens.focus_distance >= lens.hyperfocal then
+            break
+        end
+        repeat
+	        lens.focus(-1,2,false)
+	    until lens.dof_near >= target   --move the min dof focus point of the next photo to the max dof of old photo
+        target = lens.dof_far
+        takeshoot()
+    end
+
+end
+
+function takeshoot()
+local EC = AEBMenu.submenu["Ev Bracket"].value
+local shutterok = camera.shutter.value
+    console.show()
+    print("First shoot at 0EC", camera.shutter.value)
+    msleep(500)
+    shoot (64,false)
+	if EC ~= 0 then
+		if EC = 4 then
+			camera.shutter.value = shutterok * 2^(EC - 2)
+			print("Shoot at +2EV")
+			msleep(500)
+		end
+		if EC = 5 then
+			camera.shutter.value = shutterok * 2^(EC - 3)
+			print("Shoot at +2EV")
+			msleep(500)
+		end
+		camera.shutter.value = shutterok * 2^EC
+		print("Shoot at +" .. EC .. "EV ")
+		msleep(500)
+		shoot (64,false)
+		
+		if EC = 4 then
+			camera.shutter.value = shutterok / 2^(EC - 2)
+			print("Shoot at -2EV")
+			msleep(500)
+		end
+		if EC = 5 then
+			camera.shutter.value = shutterok / 2^(EC - 3)
+			print("Shoot at -2EV")
+			msleep(500)
+		end
+		camera.shutter.value = shutterok / 2^EC
+		print("second shot at -" .. EC .. "EV")
+		msleep(500)
+		shoot (64,false)
+		camera.shutter.value = shutterok
+	end
+    console.hide()
+end
