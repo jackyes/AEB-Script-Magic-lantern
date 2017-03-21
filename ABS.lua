0d35d5eb78e9e22342c27b75428d197497e51d83
jackyes <jackyes@users.noreply.github.com>
2017-03-21 08:09:27 +0100

    Update ABS.lua

--- ABS.lua
+++ ABS.lua
@@ -66,16 +66,16 @@ end
 function takeshoot()
 local shutterok = camera.shutter.value
     console.show()
-    print("first shot at %s", camera.shutter.value)
+    print("first shot at %s", shutter:__tostring())
     msleep(1000)
     shoot (64,false)
     msleep(1000)
     camera.shutter.value = shutterok * 4
-    print("second shot at 2EV %s", camera.shutter.value)
+    print("second shot at 2EV %s", shutter:__tostring())
     msleep(1000)
     shoot (64,false)
     camera.shutter.value = shutterok / 4
-    print("second shot at -2EV %s", camera.shutter.value)
+    print("second shot at -2EV %s", shutter:__tostring())
     msleep(1000)
     shoot (64,false)
     camera.shutter.value = shutterok
