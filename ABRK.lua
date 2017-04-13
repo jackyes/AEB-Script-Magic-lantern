7102fdfa4282c163b2f3856eb1339e1ba2795d71
jackyes <jackyes@users.noreply.github.com>
2017-04-13 09:09:23 +0200

    Create ABRK.lua

---
+++ ABRK.lua
@@ -0,0 +1,30 @@
+keymenu = menu.new
+{
+    name   = "Take Bracket!",
+    help   = "Bracket script.(Tv)",
+    select = function(this) task.create(main) end,
+}
+
+function main()
+	local shutterok = camera.shutter.value
+    console.show()
+	console.clear()
+    print("First shot at %s", camera.shutter.ms)
+    msleep(750)
+    shoot(false)
+    msleep(750)
+    camera.shutter.value = shutterok * 4
+	console.clear()
+    print("Second shot at 2EV %s", camera.shutter.ms)
+    msleep(750)
+    shoot(false)
+    msleep(750)
+    camera.shutter.value = shutterok / 4
+	console.clear()
+    print("Third shot at -2EV %s", camera.shutter.ms)
+    msleep(750)
+    shoot(false)
+    camera.shutter.value = shutterok
+    console.hide()
+end
+
